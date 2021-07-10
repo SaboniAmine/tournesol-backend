cac587b7e691a63a448e0abfae4492a98985e3ec
babausse <courtois.vincent@outlook.com>
2021-07-11 00:29:42 +0200

    Simplify the syntax of the test by directly calling the User model

--- tests/core/models/test_user.py
+++ tests/core/models/test_user.py
@@ -8,13 +8,6 @@ def empty_user():
     return User()
 
 
-@pytest.fixture
-def user():
-    def inner(**kwargs):
-        return User(**kwargs)
-    return inner
-
-
 class TestInvalidUsers():
     @pytest.mark.django_db
     def test_empty_user(self, empty_user):
@@ -22,21 +15,21 @@ class TestInvalidUsers():
             empty_user.full_clean()
 
     @pytest.mark.django_db
-    def test_user_without_password(self, user):
+    def test_user_without_password(self):
         with pytest.raises(ValidationError):
-            user(username="test_username").full_clean()
+            User(username="test_username").full_clean()
 
     @pytest.mark.django_db
-    def test_user_without_username(self, user):
+    def test_user_without_username(self):
         with pytest.raises(ValidationError):
-            user(password="test password").full_clean()
+            User(password="test password").full_clean()
 
     @pytest.mark.django_db
-    def test_username_with_invalid_characters(self, user):
+    def test_username_with_invalid_characters(self):
         with pytest.raises(ValidationError):
-            user(password="password", username="$test").full_clean()
+            User(password="password", username="$test").full_clean()
 
     @pytest.mark.django_db
-    def test_username_too_long(self, user):
+    def test_username_too_long(self):
         with pytest.raises(ValidationError):
-            user(password="password", username=("X"*151)).full_clean()
+            User(password="password", username=("X"*151)).full_clean()
